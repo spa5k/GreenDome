183a90599bd8fa15593545657f6d8b7cb3490423
Spark <79936503+spa5k@users.noreply.github.com>
2023-01-03 19:15:54 +0530

    feat(database): ✨ added tajweed text to quran_text table (#22)

--- packages/scraper/src/queries/insertTajweed.ts
+++ packages/scraper/src/queries/insertTajweed.ts
@@ -1,9 +1,12 @@
+import { Tajweed } from 'tajweed-ts';
 import { db } from '../db.js';
 import { TajweedRes } from '../fetchers/tajweed.js';
 
 export const insertTajweed = async (ayahs: TajweedRes[]) => {
+	const parseTajweed = new Tajweed();
 	for (const ayah of ayahs) {
-		await db.updateTable('quran').where('ayah', '=', ayah.ayah).where('surah', '=', ayah.surah).set({ 'tajweed': ayah.tajweed }).execute();
+		const parseString = parseTajweed.parse(ayah.tajweed, true);
+		await db.updateTable('quran').where('ayah', '=', ayah.ayah).where('surah', '=', ayah.surah).set({ 'tajweed': parseString }).execute();
 	}
 	console.log('Tajweed added');
 };
