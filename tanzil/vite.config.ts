3ffe4e4aaf20a1b2e6319824f7c74b7012897f2f
Spark <79936503+spa5k@users.noreply.github.com>
2023-01-04 22:14:45 +0530

    feat(tanzil): ♻️ moved tanzil from solid to react

--- tanzil/vite.config.ts
+++ tanzil/vite.config.ts
@@ -1,12 +1,11 @@
+import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 import { defineConfig } from 'vite';
 import Pages from 'vite-plugin-pages';
-import solidPlugin from 'vite-plugin-solid';
 
-// vite-env.d.ts
-/// <reference types="vite-plugin-pages/client-solid" />
+// https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [solidPlugin(), Pages()],
+	plugins: [react(), Pages()],
 	server: {
 		port: 3000,
 	},
