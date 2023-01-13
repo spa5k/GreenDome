b29703d3854f6a99fd3172ec21a0dc09cfa87c50
Spark <79936503+spa5k@users.noreply.github.com>
2023-01-13 22:22:34 +0530

    chore(refactor): 🚧 nothing

--- packages/scraper/src/index.ts
+++ packages/scraper/src/index.ts
@@ -1,13 +1,20 @@
+import { downloadSurahList } from './fetchers/chapters.js';
+import { fetchEditions } from './fetchers/editions.js';
+import { insertEditions } from './queries/insertEditions.js';
+import { insertQuranEditions } from './queries/insertQuranTextFromEditions.js';
+import { insertSurahs } from './queries/insertSurahs.js';
+import { insertTranslations } from './queries/insertTranslations.js';
+
 const main = async () => {
-	// 	const surahs = await downloadSurahList();
-	// 	await insertSurahs(surahs.chapters);
+	const surahs = await downloadSurahList();
+	await insertSurahs(surahs.chapters);
 	// const tajweed = await fetchTajweed();
 	// await insertTajweed(tajweed);
 	// const ayahInfo = await fetchAyahInfo();
 	// await insertAyahInfo(ayahInfo);
-	// const editions = await fetchEditions();
-	// await insertEditions(editions);
-	// await insertQuranEditions();
-	// await insertTranslations();
+	const editions = await fetchEditions();
+	await insertEditions(editions);
+	await insertQuranEditions();
+	await insertTranslations();
 };
 await main();
