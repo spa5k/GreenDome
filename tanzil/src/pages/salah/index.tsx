ad0a052b7c49a31a1e9ddafcdda1cb3e5a0e1ffb
Spark <79936503+spa5k@users.noreply.github.com>
2023-02-25 02:07:40 +0530

    fix(salah): 🐛 fixing the salah timing loading inconsistently

--- tanzil/src/pages/salah/index.tsx
+++ tanzil/src/pages/salah/index.tsx
@@ -2,44 +2,53 @@ import { SalahCard } from '@/components/Card/card.js';
 import { GeoLocationInfo, getLocationInfo } from '@/features/index.js';
 import { useQuery } from 'react-query';
 
-export default function PrayerTimesComponent() {
+export default function Salah() {
 	const { getLocation, prayerTimes, currentPrayer, latitude, longitude } = useSalahStore();
-	const { data } = useQuery<GeoLocationInfo>('quran_text', async () => {
-		return await getLocationInfo();
-	}, { cacheTime: 1000000 });
+	const { data } = useQuery<GeoLocationInfo>(
+		'quran_text',
+		async () => {
+			return await getLocationInfo();
+		},
+		{ cacheTime: 1000000 },
+	);
 
 	useEffect(() => {
 		getLocation();
 	}, []);
 
+	if (latitude === 1) {
+		return (
+			<div className='flex flex-col items-center justify-center gap-y-10'>
+				<IconEosIconsLoading height={100} width={100} />
+			</div>
+		);
+	}
+
 	return (
 		<div className='flex flex-col items-center justify-center gap-y-10'>
-			<p>
-				Latitude - {latitude}
-			</p>
-			<p>
-				Longitude - {longitude}
-			</p>
+			<p>Latitude - {latitude}</p>
+			<p>Longitude - {longitude}</p>
 			<div className='flex flex-wrap items-center gap-x-5'>
+				{!prayerTimes && <p>Loading...</p>}
 				{prayerTimes
-					? prayerTimes.map((prayer) => {
+					&& prayerTimes.map((prayer) => {
 						const time = new Date(prayer.time);
-						return <SalahCard key={prayer.prayer} prayer={prayer.prayer} time={time} />;
-					})
-					: (
-						<div>
-							<Button className='mt-4' onClick={() => window.location.assign(window.location.origin)}>
-								Refresh
-							</Button>
-							<p>Loading...</p>
-						</div>
-					)}
+						return (
+							<SalahCard
+								key={prayer.prayer}
+								prayer={prayer.prayer}
+								time={time}
+							/>
+						);
+					})}
 			</div>
-			<p className='mt-6 text-lg'>
-				Current Prayer: {currentPrayer}
-			</p>
+			<p className='mt-6 text-lg'>Current Prayer: {currentPrayer}</p>
 
-			{data && <p>Current location - {data.city}, {data.principalSubdivision}</p>}
+			{data && (
+				<p>
+					Current location - {data.city}, {data.principalSubdivision}
+				</p>
+			)}
 		</div>
 	);
 }
