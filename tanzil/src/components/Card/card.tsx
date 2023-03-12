e40997f25878b50b0840979e8e494997c5c55424
golam71 <mrgenius408@gmail.com>
2023-03-12 21:01:00 +0600

    make padding look good on salah card

--- tanzil/src/components/Card/card.tsx
+++ tanzil/src/components/Card/card.tsx
@@ -1,7 +1,7 @@
 export const SalahCard = ({ prayer, time }: { prayer: string; time: Date; }) => {
 	return (
-		<div className='mx-auto my-4'>
-			<div className='bg-secondary text-primary flex w-40  items-center justify-center rounded-lg  py-6  shadow-md' key={prayer}>
+		<div className='m-2 mx-auto'>
+			<div className='bg-secondary text-primary flex w-40  items-center justify-center rounded-lg  py-2  shadow-md' key={prayer}>
 				<p className='text-lg font-bold'>{prayer} </p>
 				<p className="text-lg font-bold"> : {time.toLocaleString('en-US', { hour: 'numeric', hour12: true, minute: '2-digit' })}</p>
 			</div>
