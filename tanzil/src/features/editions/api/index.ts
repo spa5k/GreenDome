9807c1cc6fe3269c9c37b22d2c387bbb32e6f2ec
Spark <79936503+spa5k@users.noreply.github.com>
2023-03-12 20:04:32 +0530

    feat(tanzil): ✨ added separate fetching functions for various editions

--- tanzil/src/features/editions/api/index.ts
+++ tanzil/src/features/editions/api/index.ts
@@ -1,61 +1,62 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
+import { formatEditionsData } from '@/features/index.js';
 import { Edition, EditionsEnum } from '@/utils/bindings.js';
+import { Err, Ok, Result } from 'ts-results';
 
+export type EditionResult = Result<Edition[], unknown>;
 abstract class EditionsAbstract {
 	isTauri = window?.__TAURI_METADATA__ ? true : false;
-	public abstract getEditions(editionType: EditionsEnum): Promise<Edition[]>;
+	public abstract getEditions(editionType: EditionsEnum): Promise<EditionResult>;
+	public abstract getTranslationEditions(): Promise<EditionResult>;
+	public abstract getTransliterationEditions(): Promise<EditionResult>;
+	public abstract getQuranTextEditions(): Promise<EditionResult>;
 }
 
 export class TauriApi extends EditionsAbstract {
-	public async getEditions(editionType: EditionsEnum): Promise<Edition[]> {
-		const { client } = await import('@/utils/rspc');
+	public async getEditions(editionType: EditionsEnum) {
+		try {
+			const { client } = await import('@/utils/rspc');
+			const data = await client.query(['editions', { edition: editionType }]);
+			return Ok(data);
+		} catch (err) {
+			return Err(err);
+		}
+	}
+	// subclasses may override these methods to provide custom implementation
+	public getTranslationEditions() {
+		return this.getEditions(EditionsEnum.Translation);
+	}
 
-		return await client.query(['editions', { edition: editionType }]);
+	// subclasses may override these methods to provide custom implementation
+	public getTransliterationEditions() {
+		return this.getEditions(EditionsEnum.Transliteration);
+	}
+
+	// subclasses may override these methods to provide custom implementation
+	public getQuranTextEditions() {
+		return this.getEditions(EditionsEnum.Quran);
 	}
 }
 
 class QuranApi extends EditionsAbstract {
-	public async getEditions(editionType: EditionsEnum): Promise<Edition[]> {
-		const { $fetch } = await import('ohmyfetch');
-		const data = await $fetch('https://cdn.jsdelivr.net/gh/fawazahmed0/quran-api@1/editions.min.json');
-		const formattedData = this.formatData(data, editionType);
+	public async getEditions(editionType: EditionsEnum) {
+		try {
+			const { $fetch } = await import('ohmyfetch');
+			const data = await $fetch('https://cdn.jsdelivr.net/gh/fawazahmed0/quran-api@1/editions.min.json');
+			const formattedData = formatEditionsData(data, editionType);
 
-		return formattedData;
-	}
-
-	formatData(editionsData: any, editionType: EditionsEnum): Edition[] {
-		const editions: string[] = Object.keys(editionsData);
-		const finalEditions: Edition[] = [];
-
-		for (let index = 0; index < editions.length; index++) {
-			const edition = editions[index];
-			const editionInfo = editionsData[edition];
-
-			const keys: Record<Type, EditionsEnum> = {
-				quran: 'Quran',
-				quran_transliteration: 'Transliteration',
-				translation: 'Translation',
-				transliteration: 'Transliteration',
-			};
-			const editionVariant = getEdition(edition);
-			if (keys[editionVariant] !== editionType) {
-				continue;
-			}
-
-			const editionFinal: Edition = {
-				author: editionInfo?.author || '',
-				direction: editionInfo.direction,
-				language: editionInfo.language,
-				name: editionInfo.name,
-				type: editionVariant,
-				source: editionInfo.source,
-				enabled: 'true',
-				id: index + 1,
-			};
-
-			finalEditions.push(editionFinal);
+			return Ok(formattedData);
+		} catch (err) {
+			return Err(err);
 		}
-		return finalEditions;
+	}
+	public getTranslationEditions() {
+		return this.getEditions(EditionsEnum.Translation);
+	}
+	public getTransliterationEditions() {
+		return this.getEditions(EditionsEnum.Transliteration);
+	}
+	public getQuranTextEditions() {
+		return this.getEditions(EditionsEnum.Quran);
 	}
 }
 /**
@@ -84,42 +85,45 @@ export class EditionsApi extends EditionsAbstract {
 	/**
 	 * Gets the editions of a given type.
 	 * @param {EditionsEnum} editionType The type of editions to get.
-	 * @returns {Edition[]} The editions of the given type.
+	 * @returns The editions of the given type.
 	 */
-	public getEditions(editionType: EditionsEnum): Promise<Edition[]> {
+	public getEditions(editionType: EditionsEnum) {
 		return this.helper.getEditions(editionType);
 	}
-}
-
-/**
- * @param edition - The string to be matched against the regexes
- * @returns The Type of the edition
- * @description
- * - If the edition starts with 'ara_quran' and includes 'la', then it is a quran transliteration
- * - If the edition starts with 'ara_quran' and does not include 'la', then it is a quran
- * - If the edition does not start with 'ara_quran' and includes 'la', then it is a transliteration
- * - Otherwise, it is a translation
- * - returns the Type of the string
- */
-const getEdition = (edition: string): Type => {
-	const isQuranTransliteration = /^ara_quran.*la$/;
-	const isGenericTransliteration = /^(?!ara_quran).*(la$|la*$)/;
-	const isQuran = /^ara_quran(?!.*(la\d*)$)/;
-	if (edition.match(isQuranTransliteration) || edition.startsWith('ara_quran') && edition.includes('la')) {
-		return Type.QURAN_TRANSLITERATION;
+	/**
+	 * Gets the translation editions.
+	 * @returns The translation editions.
+	 * @example
+	 * ```ts
+	 * const editionsApi = new EditionsApi();
+	 * const editions = await editionsApi.getTranslationEditions();
+	 * ```
+	 */
+	public async getTranslationEditions() {
+		return await this.helper.getEditions(EditionsEnum.Translation);
 	}
-	if (edition.match(isQuran) || edition.startsWith('ara_quran') && !edition.includes('la')) {
-		return Type.QURAN;
+	/**
+	 * Gets the transliteration editions.
+	 * @returns The transliteration editions.
+	 * @example
+	 * ```ts
+	 * const editionsApi = new EditionsApi();
+	 * const editions = await editionsApi.getTransliterationEditions();
+	 * ```
+	 */
+	public async getTransliterationEditions() {
+		return await this.helper.getEditions(EditionsEnum.Transliteration);
 	}
-	if (edition.match(isGenericTransliteration) && !edition.startsWith('ara_quran') && edition.includes('la')) {
-		return Type.TRANSLITERATION;
+	/**
+	 * Gets the quran editions.
+	 * @returns The quranText editions.
+	 * @example
+	 * ```ts
+	 * const editionsApi = new EditionsApi();
+	 * const editions = await editionsApi.getQuranEditions();
+	 * ```
+	 */
+	public async getQuranTextEditions() {
+		return await this.helper.getEditions(EditionsEnum.Quran);
 	}
-	return Type.TRANSLATION;
-};
-
-enum Type {
-	TRANSLATION = 'translation',
-	QURAN = 'quran',
-	TRANSLITERATION = 'transliteration',
-	QURAN_TRANSLITERATION = 'quran_transliteration',
 }
