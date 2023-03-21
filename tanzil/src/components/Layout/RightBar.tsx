6371470c1aeb239c1103222d16e7d7dddbf0f516
Spark <79936503+spa5k@users.noreply.github.com>
2023-03-21 22:19:27 +0530

    updated code with hijri and salah times (on rightbar) (#181)
    Co-authored-by: golam71 <mrgenius408@gmail.com>

--- tanzil/src/components/Layout/RightBar.tsx
+++ tanzil/src/components/Layout/RightBar.tsx
@@ -1,7 +1,11 @@
-export default function RightBar() {
+
+// eslint-disable-next-line no-restricted-imports
+import Salah from '@/features/salah/Salah.js'; //import path can not end with .ts or .tsx
+
+export default function RightBar(): JSX.Element {
 	return (
 		<div className='h-full basis-1/4'>
-			<p>This is the right bar</p>
+			<Salah /> 
 		</div>
 	);
 }
