70a91e733f76617d6d12f5162e2320b2e74f06c5
golam71 <mrgenius408@gmail.com>
2023-03-25 15:57:04 +0600

    jsdoc documentation

--- tanzil/src/features/hijri/Hijri.ts
+++ tanzil/src/features/hijri/Hijri.ts
@@ -1,13 +1,20 @@
-export default function getHijriDate(language: string): string {
-	const languageTag = language === 'en' ? 'en-TN-u-ca-islamic-nu-eng' : 'ar-TN-u-ca-islamic-nu-ar';
-	const hijriFormatter = new Intl.DateTimeFormat(languageTag, {
+/**
+
+    Get the current Hijri date formatted in the specified language
+    @param {string} language - The language code to use for the formatted date. Accepted values: 'en' for English and 'ar' for Arabic.
+    @returns {string} The formatted Hijri date as a string.
+    */
+    export default function getHijriDate(language) {
+		const languageTag = language === 'en' ? 'en-TN-u-ca-islamic-nu-eng' : 'ar-TN-u-ca-islamic-nu-ar';
+		const hijriFormatter = new Intl.DateTimeFormat(languageTag, {
 		day: 'numeric',
 		month: 'long',
 		weekday: 'long',
 		year: 'numeric',
-	});
-	return hijriFormatter.format(Date.now());
-}
-
-// const hijriDateInArabic = getHijriDate('ar'); // returns the Hijri date in Arabic
-// const hijriDateInEnglish = getHijriDate('en'); // returns the Hijri date in English
+		});
+		return hijriFormatter.format(Date.now());
+		}
+	
+	// Example usage:
+	// const hijriDateInArabic = getHijriDate('ar'); // returns the Hijri date in Arabic
+	// const hijriDateInEnglish = getHijriDate('en'); // returns the Hijri date in English
