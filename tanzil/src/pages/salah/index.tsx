e872f72e771c5b992b255ee8db981aebd7b331a2
golam71 <mrgenius408@gmail.com>
2023-03-31 17:39:51 +0600

    add options

--- tanzil/src/pages/salah/index.tsx
+++ tanzil/src/pages/salah/index.tsx
@@ -2,6 +2,29 @@ import { GeoLocationInfo, getLocationInfo } from '@/features/index.js';
 import { useQuery } from '@tanstack/react-query';
 import { Key } from 'react';
 
+
+// const madhabOptions = [
+//   { label: 'Shafi', value: adhan.Madhab.Shafi },
+//   { label: 'Hanafi', value: adhan.Madhab.Hanafi },
+// ];
+
+// const calculationMethodOptions = [
+//   { label: 'Muslim World League', value: adhan.CalculationMethod.MuslimWorldLeague() },
+//   { label: 'Egyptian', value: adhan.CalculationMethod.Egyptian() },
+//   { label: 'Karachi', value: adhan.CalculationMethod.Karachi() },
+//   { label: 'Umm Al-Qura', value: adhan.CalculationMethod.UmmAlQura() },
+//   { label: 'Dubai', value: adhan.CalculationMethod.Dubai() },
+//   { label: 'Qatar', value: adhan.CalculationMethod.Qatar() },
+//   { label: 'Kuwait', value: adhan.CalculationMethod.Kuwait() },
+//   { label: 'Moonsighting Committee', value: adhan.CalculationMethod.MoonsightingCommittee() },
+//   { label: 'Singapore', value: adhan.CalculationMethod.Singapore() },
+//   { label: 'Turkey', value: adhan.CalculationMethod.Turkey() },
+//   { label: 'Tehran', value: adhan.CalculationMethod.Tehran() },
+//   { label: 'North America', value: adhan.CalculationMethod.NorthAmerica() },
+//   { label: 'Other', value: adhan.CalculationMethod.Other() },
+// ];
+
+
 export default function Salah() {
 	const { getLocation, prayerTimes, currentPrayer, latitude, longitude } = useSalahTrackedStore();
 	const { data } = useQuery<GeoLocationInfo>(
@@ -25,8 +48,8 @@ export default function Salah() {
 
 	return (
 		<div className='flex flex-col items-center justify-center gap-y-10'>
-			<p>Latitude - {latitude}</p>
-			<p>Longitude - {longitude}</p>
+			{/* <p>Latitude - {latitude}</p>
+			<p>Longitude - {longitude}</p> */}
 			<div className='flex flex-wrap items-center gap-x-5'>
 				{!prayerTimes && <p>Loading...</p>}
 				{prayerTimes
@@ -43,11 +66,12 @@ export default function Salah() {
 			</div>
 			<p className='mt-6 text-lg'>Current Prayer: {currentPrayer}</p>
 
-			{data && (
+			{/* {data && (
 				<p>
 					Current location - {data.city}, {data.principalSubdivision}
 				</p>
-			)}
+			)} */}
+      {console.log (data,longitude,latitude)}
 		</div>
 	);
 }
